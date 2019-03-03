1a15a2b78280c0776fde9641806088708fb00651
faxedexcel <chinh.le23@gmail.com>
2019-03-03 17:47:06 -0500

    completed advanced mode, still need to write and run tests

--- pathfinder.py
+++ pathfinder.py
@@ -23,9 +23,11 @@ class Map:
         self.height = len(self.intensities)
         self.map_image = self.map_drawer.draw_map(self.width, self.height, self.intensities)
         self.optimal_path = self.path_finder.find_path(self.y_coord, self.elevations)
-        self.optimal_path_multiple = self.path_finder.find_path_multiple(self.height, self.elevations)
+        self.optimal_path_all = self.path_finder.find_path_all(self.height, self.elevations)
         self.optimal_path_image = self.map_drawer.draw_optimal_path(self.map_image, self.optimal_path)
-        self.optimal_path_image_multiple = self.map_drawer.draw_optimal_path_multiple(self.map_image, self.optimal_path_multiple)
+        self.optimal_path_image_all = self.map_drawer.draw_optimal_path_all(self.map_image, self.optimal_path_all)
+        self.best_optimal_path_all = self.path_finder.find_best_path(self.optimal_path_all, self.elevations)
+        self.best_optimal_path_image = self.map_drawer.draw_optimal_path(self.optimal_path_image_all, self.best_optimal_path_all)
 
     def convert_to_list(self):
         """
@@ -75,14 +77,14 @@ class MapDrawer:
         Given a list of optimal coordinates, draw a line displaying the optimal path onto an elevation map image
         """
         for coordinate in optimal_coordinates:
-            map_image.putpixel(coordinate, ImageColor.getcolor('green', 'RGBA')) 
+            map_image.putpixel(coordinate, ImageColor.getcolor('lime', 'RGBA')) 
         return map_image
 
-    def draw_optimal_path_multiple(self, map_image, mulitple_optimal_coordinates):
+    def draw_optimal_path_all(self, map_image, all_optimal_coordinates):
         """
         Given a 2-D list of optimal coordinates, draw all optimal paths onto an elevation map image
         """
-        for line in mulitple_optimal_coordinates:
+        for line in all_optimal_coordinates:
             for coordinate in line:
                 map_image.putpixel(coordinate, ImageColor.getcolor('cyan', 'RGBA')) 
         return map_image
@@ -132,25 +134,63 @@ class PathFinder:
             x += 1    
             optimal_path.append((x, y))
         return optimal_path
+
+    def find_best_path(self, all_paths, elevations_list):
+        """
+        Given a list of all possible paths and list of elevations, 
+        return the path with the least total elevation change
+        """
+        # create list of [path/elevation pairs:[coordinates[(x, y),..], elevations[<ints>,..]],..]
+        path_elevation_list = []
+        for path in all_paths:
+            elevations = []
+            for coord in path:
+                elevations.append(elevations_list[coord[1]][coord[0]])
+            path_elevation_list.append([path, elevations])
+        
+        # create list of [path/elevation-change pairs: [coordinates[(x,y),..], elevation-changes[<ints>,..],..],..]
+        path_elev_diffs = []
+        for item in path_elevation_list:
+            elevation_difference = []
+            for elevation in range(len(item[1])-1):
+                elevation_difference.append(abs(item[1][elevation] - item[1][elevation + 1]))
+            path_elev_diffs.append([item[0], elevation_difference])
+
+        # create list of [[paths:[coordinates[(x,y),..], <total elevation change>],..]
+        path_elev_change_sums = []
+        for item in path_elev_diffs:
+            total_change = 0
+            for diff in range(len(item[1])):
+                total_change += item[1][diff]
+            path_elev_change_sums.append([item[0], total_change])
+
+        # determine the path with the lowest total elevation change
+        min_elev_change = path_elev_change_sums[0][1]
+        path_least_elevation_change = []
+        for item in range(len(path_elev_change_sums)):
+            if path_elev_change_sums[item][1] < min_elev_change:
+                min_elev_change = path_elev_change_sums[item][1]
+                path_least_elevation_change = path_elev_change_sums[item][0] 
+
+        return path_least_elevation_change
     
-    def find_path_multiple(self, height, elevations):
+    def find_path_all(self, height, elevations):
         """
         Given a map height and 2-D list of elevations,
         return a list of all possible paths of least resistance from west to east
         """
-        optimal_path_multiple = []
+        optimal_path_all = []
         for i in range(height):
-            optimal_path_multiple.append(self.find_path(i, elevations))
-        return optimal_path_multiple
+            optimal_path_all.append(self.find_path(i, elevations))
+        return optimal_path_all
 
 
 if __name__ == "__main__":
     map_info = MapDrawer()
     optimal_path_info = PathFinder()
-    optimal_path_map_multiple = Map("elevation_small.txt", map_info, optimal_path_info)
-    optimal_path_image_multiple = optimal_path_map_multiple.optimal_path_image_multiple
-    optimal_path_image_multiple.save('multiple_paths_test.png')
-    
+    optimal_path_map_all = Map("elevation_small.txt", map_info, optimal_path_info)
+    best_optimal_path_image = optimal_path_map_all.best_optimal_path_image
+    best_optimal_path_image.save('best_optimal_path_image_test.png')
 
 
 
